1236ba961b1d3db2c2d38c2b5563e1af55443df1
Nathan Loveless <nathansl2003@yahoo.com>
2020-07-09 17:47:40 -0400

    commiting lecture code

---
+++ pythonI.py
@@ -0,0 +1,31 @@
+# num = 123
+# name = 'Nathan'
+# line = 'Hello ' + name
+# print(line)
+
+# List
+# lst = [1, 4, 10, 10, "cat", 10, 23]
+# lst.append(3.5)
+# lst.append([100, 200, 300])
+# lst.insert(2, "hello")
+# print(lst)
+
+# For loops
+# for elm in lst:
+#     print(elm)
+
+# for i in range(6):
+#     print(lst[i]) 
+
+# Dictionary
+dic = {"foo": 12, "bar": 34}
+print(dic)
+val= dic["foo"]
+print(val)
+
+# format string
+print(f'garbage {val}')
+
+num=1010.256
+print(f'value {num}')
+
